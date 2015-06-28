6275e119ef5eddb5f4d806be546820633921600d
Marton Braun <zsmb13@sch.bme.hu>
2015-06-28 11:18:00 +0200

    Added bubble sort, selection sort and gnome sort

--- main.cpp
+++ main.cpp
@@ -4,6 +4,50 @@
 const int WIDTH = 1280;
 const int HEIGHT = 720;
 
+void buborek(double t[], int db) {
+   int i, j;
+ 
+   /* egyre rövidebb tömbrészletek ciklusa */
+   for (i = db-1; i > 0; --i)
+      /* egymás utáni párok ciklusa */
+      for (j = 0; j < i; ++j)
+         if (t[j+1] < t[j]) {      /* összehasonlítás */
+            double temp = t[j];
+            t[j] = t[j+1];         /* csere */
+            t[j+1] = temp;
+         }
+}
+
+void kozvetlen(double t[], int db) {
+   int i, j, minindex;
+ 
+   for (i = 0; i < db-1; ++i) {
+      minindex = i;                /* minimum keresése */
+      for (j = i+1; j < db; ++j)
+         if (t[j] < t[minindex])
+            minindex = j;
+      if (minindex != i) {         /* csere? */
+         double temp = t[minindex];
+         t[minindex] = t[i];       /* csere. */
+         t[i] = temp;
+      }
+   }
+}
+
+void torperendez(double t[], int db) {
+    int i = 0;
+    while (i < db) {
+        if (i == 0 || t[i-1] <= t[i]) { /* jó sorrend? */
+            i++;                /* előre */
+        } else {
+            double tmp = t[i];  /* csere */
+            t[i] = t[i-1];
+            t[i-1] = tmp;
+            i--;                /* vissza */
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     SDL_Event ev;
     SDL_Window *sdlWindow;
